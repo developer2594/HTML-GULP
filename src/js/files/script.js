2129865b4ecba2dc43e184778b955533186e6f73
farrukh <developer2594@gmail.com>
2023-04-14 23:48:26 +0200

    no end list

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,19 +2,13 @@
 // import { isMobile } from "./functions.js";
 import data from "../persons.js";
 window.addEventListener("DOMContentLoaded", (event) => {
-  const persons = document.querySelector(".product-content__info");
-  let persone;
-
-  // persons.appendChild(html);
+  const tBody = document.querySelector(".product-content__body");
+  let key = "";
   // =================================
-  data.forEach((persone) => {
-    create(persone);
-    // let html = document.createElement(tegs);
-    // console.log("persons", persons);
-    // persons.appendChild(html);
-  });
-  function create(persone) {
-    //   let tegs = `<td>${persone.name}</td>
+  data.forEach((person, i, array) => {
+    // persons.innerHTML = `<td>${person}</td>`;
+    //   let tegs = `<div>${persone.id}</div>
+    // <td>${persone.name}</td>
     // <td>${persone.company}</td>
     // <td>${persone.phone}</td>
     // <td>${persone.email}</td>
@@ -24,10 +18,57 @@ window.addEventListener("DOMContentLoaded", (event) => {
     //     ${persone.status}
     //   </button>
     // </td>`;
-    let tegs = document.createElement("td");
-    tegs.innerHTML = `<div></div>`;
-    // let html = document.createElement(tegs);
-    persons.appendChild(tegs);
-    console.log("persone", persons);
+  });
+
+  for (key in data) {
+    const person = document.createElement("tr"),
+      name = document.createElement("td"),
+      company = document.createElement("td"),
+      phone = document.createElement("td"),
+      email = document.createElement("td"),
+      country = document.createElement("td"),
+      status = document.createElement("td"),
+      tdButton = document.createElement("td"),
+      button = document.createElement("button");
+    // let tag = {};
+
+    person.classList.add("product-content__person");
+    let tag = `<td class="product-content__name">${data[key].name}</td>
+    <td class="product-content__company">${data[key].company}</td>
+    <td class="product-content__phone">${data[key].phone}</td>
+    <td class="product-content__email">${data[key].email}</td>
+    <td class="product-content__country">${data[key].country}</td>
+    <td class="product-content__button">
+    <button class="product-content__btn">${data[key].status}</button>
+    </td>`;
+    // name.classList.add("product-content__name");
+    // company.classList.add("product-content__company");
+    // phone.classList.add("product-content__phone");
+    // email.classList.add("product-content__email");
+    // country.classList.add("product-content__country");
+    // tdButton.classList.add("product-content__button");
+    // button.classList.add("product-content__btn");
+
+    // name.innerHTML = `${data[key].name}`;
+    // company.innerHTML = `${data[key].company}`;
+    // phone.innerHTML = `${data[key].phone}`;
+    // email.innerHTML = `${data[key].email}`;
+    // country.innerHTML = `${data[key].country}`;
+
+    // tdButton.appendChild(button);
+    // person.appendChild(name);
+    person.innerHTML = tag;
+    // person.appendChild(company);
+    // person.appendChild(phone);
+    // person.appendChild(email);
+    // person.appendChild(country);
+    // person.appendChild(status);
+    tBody.appendChild(person);
+
+    // console.log("key:", data[key].name);
+    console.log("tag:", tag);
   }
+  // for (let i = 0; i < data.length; i++) {
+  //   console.log("data", data[i]);
+  // }
 });
