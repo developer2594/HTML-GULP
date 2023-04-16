9dd75a184888a921a4b9cc096af4580c87fc0c14
farrukh <developer2594@gmail.com>
2023-04-16 18:54:55 +0200

    start

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,30 +1,4 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
 // import { isMobile } from "./functions.js";
-import data from "../persons.js";
-window.addEventListener("DOMContentLoaded", (event) => {
-  const tBody = document.querySelector(".product-content__body"),
-    entries = document.querySelector(".navigation-length__entries");
-  let personLength = data.length;
 
-  entries.innerHTML = `${personLength}`;
 
-  data.forEach((person) => {
-    const tag = `
-    <td class="product-content__name">${person.name}</td>
-    <td class="product-content__company">${person.company}</td>
-    <td class="product-content__phone">${person.phone}</td>
-    <td class="product-content__email">${person.email}</td>
-    <td class="product-content__country">${person.country}</td>
-    <td class="product-content__button">
-    <button class="product-content__btn">${person.status}</button>
-    </td>
-    `;
-    const persone = document.createElement("tr");
-    persone.classList.add("product-content__person");
-
-    personLength = person.length;
-
-    persone.innerHTML = tag;
-    tBody.appendChild(persone);
-  });
-});
