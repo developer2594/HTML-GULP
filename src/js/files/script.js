36b8694d2afb17b270bbfd61c2600ae1dca266ab
dev <dev@MacBook-Pro-dev.local>
2023-04-27 08:13:49 +0200

    past

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,58 +1,48 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
-//import { isMobile } from "./functions.js";
-//========================================================================================================================================================
+// import { isMobile } from "./functions.js";
+import data from "../persons.js";
+window.addEventListener("DOMContentLoaded", (event) => {
+  const tBody = document.querySelector(".content-info__body"),
+    entries = document.querySelector(".navigation-length__entries");
+  let personLength = data.length;
 
-document.addEventListener("click", documentAction);
-// проверка дляны элементов в блоке и проставление класса с номером длины элемента
-const menuBlocks = document.querySelectorAll('.sub-menu-catalog__block');
-if (menuBlocks.length) {
-   menuBlocks.forEach(menuBlock => {
-      const menuBlockItems = menuBlock.querySelectorAll('.sub-menu-catalog__category').length;
-      menuBlock.classList.add(`sub-menu-catalog__block_${menuBlockItems}`);
-   });
-}
-//========================================================================================================================================================
-// открытие и закрытие мены по дата данным
-function documentAction(e) {
-   const targetElement = e.target;
-   if (targetElement.closest('[data-parent]')) {
-      const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
-      const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+  const list = document.querySelectorAll(".content-info__list");
+  list.forEach((item) => {
+    const ul = document.createElement("ul");
+    item.appendChild(ul);
+  });
 
-      if (subMenu) {
-         const activeLink = document.querySelector('._sub-menu-active');
-         const activeBlock = document.querySelector('._sub-menu-open');
-         if (activeLink && activeLink === targetElement) {
-            //activeBlock.classList.remove('._sub-menu-open');
-            //activeLink.classList.remove('._sub-menu-active');
-            document.documentElement.classList.remove('._sub-menu-open');
-            document.documentElement.classList.remove('._sub-menu-active');
-            document.documentElement.classList.remove('sub-menu-open');
-         }
-         document.documentElement.classList.toggle('sub-menu-open');
-         targetElement.classList.toggle('_sub-menu-active');
-         subMenu.classList.toggle('_sub-menu-open');
-      } else {
-         console.log('Ой ой, нет такого подменю :(');
-      }
-      e.preventDefault();
-   }
-   if (targetElement.closest('.menu-top-header__link_catalog')) {
-      document.documentElement.classList.add('catalog-open');
-      e.preventDefault();
-   }
-   if (targetElement.closest('.menu-catalog__back')) {
-      document.documentElement.classList.remove('catalog-open');
-      document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
-      document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
-      e.preventDefault();
-   }
-   if (targetElement.closest('.sub-menu-catalog__back')) {
-      document.documentElement.classList.remove('sub-menu-open');
-      document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
-      document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
-      e.preventDefault();
-   }
+  list.forEach((i) => {
+    data.forEach((person) => {
+      i.querySelectorAll("ul").id = `${person.name}`;
+      console.log("person", person.name, i);
+    });
+    console.log("i", i.querySelectorAll("ul"));
+  });
 
-}
-//========================================================================================================================================================
+  entries.innerHTML = `${personLength}`;
+  data.forEach((person) => {
+    const tag = `
+    <div class="content-info__name">${person.name}</div>
+    <div class="content-info__company">${person.company}</div>
+    <div class="content-info__phone">${person.phone}</div>
+    <div class="content-info__email">${person.email}</div>
+    <div class="content-info__country">${person.country}</div>
+    <div class="content-info__button">
+    <button class="content-info__btn">${person.status}</button>
+    </div>
+    `;
+    // const ul = (list = document.querySelectorAll("ul"));
+    // ul.id = `${person.name}`;
+    // console.log(ul);
+    personLength = person.length;
+
+    // persone.innerHTML = tag;
+    // tBody.appendChild(persone);
+    // creatUl(name);
+  });
+  function creatUl(name) {
+    const ul = (list = document.querySelectorAll("ul"));
+    ul.id = aaa;
+  }
+});
